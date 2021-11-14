a8328acf15b4e8071ef879684d095ad8600e2f4f
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2021-11-14 18:39:16 +0000

    done with playerRound function

---
+++ rcs.js
@@ -0,0 +1,42 @@
+function computerPlay() {
+    const choices = ['scissors', 'paper', 'rock'];
+    return choices[Math.floor(Math.random() * choices.length)];
+
+}
+
+// console.log(computerPlay())
+
+function playRound(player, computer) {
+
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'computer wins'
+
+        } else if (computerSelection === 'scissors') {
+            return 'player wins'
+        }
+
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'computer wins'
+
+        } else if (computerSelection === 'rock') {
+            return 'player wins'
+        }
+
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'computer wins'
+
+        } else if (computerSelection === 'rock') {
+            return 'player wins'
+        }
+
+    }
+}
+
+const playerSelection = prompt('choose one: rock / paper / scissors');
+const computerSelection = computerPlay()
+
+console.log(playRound(playerSelection, computerSelection))
