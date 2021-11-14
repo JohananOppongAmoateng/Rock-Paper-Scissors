7976697b993300770626f48a6911c72004333ce0
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2021-11-14 19:53:58 +0000

    version 1

--- rcs.js
+++ rcs.js
@@ -8,35 +8,56 @@ function computerPlay() {
 
 function playRound(player, computer) {
 
-
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
+    if (player === 'rock') {
+        if (computer === 'paper') {
             return 'computer wins'
 
-        } else if (computerSelection === 'scissors') {
+        } else if (computer === 'scissors') {
             return 'player wins'
         }
 
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'scissors') {
+    } else if (player === 'paper') {
+        if (computer === 'scissors') {
             return 'computer wins'
 
-        } else if (computerSelection === 'rock') {
+        } else if (computer === 'rock') {
             return 'player wins'
         }
 
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
+    } else if (player === 'scissors') {
+        if (computer === 'rock') {
             return 'computer wins'
 
-        } else if (computerSelection === 'paper') {
+        } else if (computer === 'paper') {
             return 'player wins'
         }
 
+    } else {
+        return 'It was a tie'
     }
 }
 
-const playerSelection = prompt('choose one: rock / paper / scissors');
-const computerSelection = computerPlay()
 
-console.log(playRound(playerSelection, computerSelection))
+let comp_score = 0
+let player_score = 0
+
+function game() {
+    let playerSelection = prompt('choose one: rock / paper / scissors');
+    let computerSelection = computerPlay()
+
+    let winner = playRound(playerSelection, computerSelection)
+    console.log(winner)
+
+    if (winner === 'computer wins') {
+        comp_score += 1;
+    } else if (winner === 'player wins') {
+        player_score += 1;
+    } else {
+        player_score += 0
+        comp_score += 0
+    }
+
+}
+game(game(game(game(game()))))
+
+console.log("The score is " + comp_score + ':' + player_score)
